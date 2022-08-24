79676321aef98f22a1bcb6571d5adbe9d7b5aad7
Albert Krewinkel <albert@zeitkraut.de>
2022-08-24 15:49:24 +0200

    Recreate links for obfuscated mail addresses with JS

--- _extensions/css-mailto-obfuscation.lua
+++ _extensions/css-mailto-obfuscation.lua
@@ -1,3 +1,23 @@
+local js = [[
+<script type="text/javascript">
+window.document.addEventListener('DOMContentLoaded', _ => {
+    const mailAddressClass = 'obfuscated-mail-address';
+    window.document.querySelectorAll('.' + mailAddressClass).forEach(el => {
+      const address = [
+          el.getAttribute('data-user') || '',
+          '@',
+          el.getAttribute('data-domain') || ''
+      ].join('');
+      const link = window.document.createElement('a');
+      link.setAttribute('href', 'mailto:' + address);
+      link.textContent = address;
+      el.replaceChildren(link);
+      el.classList.remove(mailAddressClass);
+  });
+});
+</script>
+]]
+
 local css = [[
 <style>
 /* Avoiding Spam */
@@ -11,7 +31,13 @@ local css = [[
 </style>
 ]]
 
-function Meta (meta)
+local stringify = pandoc.utils.stringify
+local has_mail = false
+
+local function Meta (meta)
+  if not has_mail then
+    return nil
+  end
   local header_includes = meta['header-includes']
   if pandoc.utils.type(header_includes) == 'nil' then
     header_includes = pandoc.List{}
@@ -19,13 +45,15 @@ function Meta (meta)
     header_includes = pandoc.List{header_includes}
   end
   header_includes:insert({pandoc.RawBlock('html', css)})
+  header_includes:insert({pandoc.RawBlock('html', js)})
   meta['header_includes'] = header_includes
   return meta
 end
 
-function Link (link)
+local function Link (link)
   local user, domain = link.target:match 'mailto:([^%@]*)%@(.*)$'
-  if user and domain then
+  if user and domain and stringify(link.content) == link.target then
+    has_mail = true
     return pandoc.Span(
       pandoc.Emph 'obfuscated mail address',
       {
@@ -36,3 +64,8 @@ function Link (link)
     )
   end
 end
+
+return {
+  {Link = Link},
+  {Meta = Meta}
+}
